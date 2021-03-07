241eb39140fdcf9275eb35b12086fd298ce9ba62
Mathys Ferriere <mathys.ferriere@gmail.com>
2021-03-07 17:01:58 +0100

    Un second commit

--- ProjetV1.py
+++ ProjetV1.py
@@ -5,20 +5,48 @@ Created on Tue Mar  2 16:53:27 2021
 @author: math2
 """
 
+### Test Commit
+
+print("Bienvenue dans notre nouveau Projet")
+
 class Order:
-    def __init__(self,typ,quantity,price):
-        self.typ = typ
+    compteur=0
+    def __init__(self,quantity,price):
         self.quantity = quantity
         self.price = price
+        Order.compteur += 1
+        self.cpt = Order.compteur
 
 class Book:
-    def __init__(self,order):
-        self.order = order
+    def __init__(self,name):
+        self.name = name
+        self.bookorder = []
+    
+    def Affichage(self,typ,order):
+        print("--- Insert ", typ, order.quantity,"@",order.price, "id=", order.cpt, "on", self.name)
+     
+    def AffichageBook(self,typ,order):
+        print("Book on", self.name)
+        n = len(self.bookorder)
+        for i in range(n):
+            print("     ",typ,self.bookorder[i].quantity,"@",self.bookorder[i].price, "id=", self.bookorder[i].cpt)
+        print("-----------------------------------")
+                   
+    def insert_buy(self,order):
+        self.bookorder.append(order)
+        Book.Affichage(self,"BUY",order)
+        Book.AffichageBook(self,"BUY",order)
+    
+    def insert_sell(self,order):
+        self.bookorder.append(order)
+        Book.Affichage(self,"SELL",order)
+        Book.AffichageBook(self,"SELL",order)
         
-    def insert_buy(self,quantity,price):
-        return 0
     
-    def insert_sell(self,quantity,price):
-        return 0
-    
-    
+        
+if __name__ == "__main__":
+    myfirstorder = Book("TEST")
+    order1 = Order(10,10.0)
+    myfirstorder.insert_buy(order1)
+    order2 = Order(120,12.0)
+    myfirstorder.insert_sell(order2)
